8054519e1d35ae1e0f6c6c47e0f32edddd30543e
Arjun Gautam <Arjungautam1051@gmail.com>
2026-04-02 19:00:47 +0530

    Change Google Maps iframe source location
    Updated the Google Maps iframe source to a new location.

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -123,17 +123,17 @@ const Contact: React.FC = () => {
 
                 {/* Map Placeholder */}
                 <div className="aspect-video rounded-xl overflow-hidden bg-muted">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241317.11609823277!2d72.74109995!3d19.08219865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c6306644edc1%3A0x5da4ed8f8d648c69!2sMumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1234567890"
-                    width="100%"
-                    height="100%"
-                    style={{ border: 0 }}
-                    allowFullScreen
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                    title="Matica.life Location"
-                  />
-                </div>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241317.11609823277!2d78.05554677598198!3d27.599001934855888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c6306644edc1%3A0x5da4ed8f8d648c69!2s204101!5e0!3m2!1sen!2sin!4v1234567890"
+    width="100%"
+    height="100%"
+    style={{ border: 0 }}
+    allowFullScreen
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+    title="Matica.life Location"
+  />
+</div>
               </div>
 
               {/* Contact Form */}
